45e8dd4e8fa51142767ae3e7abdf020e6f2d8353
carotheuri <princesstheush2@gmail.com>
2023-08-11 07:01:38 +0300

    creating project class

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -1,4 +1,23 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Project {
-    public Project() {
+    private String Department;
+    private String assetName;
+    private String productOwner;
+    private String productOwner;
+    private String productOwner;
+    private String productOwner;private String productOwner;private String productOwner;private String productOwner;
+
+
+
+
+    private static ArrayList<Project> instances = new ArrayList<>();
+    private boolean active;
+    private LocalDateTime createdAt;
+    private int id;
+    public Project(String Department,String assetName, String productOwner, String projectDescription, String gitLink,String projectBenefit,String deployedServer, String commonIssues) {
+
+
     }
 }
