ce30cc919932a960d08c44af027c7f6879b6b327
carotheuri <princesstheush2@gmail.com>
2023-08-11 05:06:24 +0300

    adding a project form template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,11 +8,16 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        get("/projects/new", (request, response) -> { //URL to make new project on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "newasset-form.hbs");
+        }, new HandlebarsTemplateEngine());
 //        <view the homepage >
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
 //            model.put("content",request.session().attribute("content"));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
     }
 }
