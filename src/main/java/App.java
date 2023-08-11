aee56995687f0c8a3e21c13001e87b9e667d80b0
carotheuri <princesstheush2@gmail.com>
2023-08-11 03:41:17 +0300

    creating a navbar hbs file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+//        <view the homepage >
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+//            model.put("content",request.session().attribute("content"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
